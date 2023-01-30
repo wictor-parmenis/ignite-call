c5d08c74546a9cb293663fb7bb03232135afc843
wictor-parmenis <wictortec@gmail.com>
2023-01-30 09:39:01 -0300

    fix: fix return in const calendar weeks

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -74,6 +74,9 @@ export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   )
 
   const calendarWeeks = useMemo(() => {
+    if (!blockedDates) {
+      return []
+    }
     const daysInMonthArray = Array.from({
       length: currentDate.daysInMonth(),
     }).map((_, i) => {
